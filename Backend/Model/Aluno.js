020d7380a94203d0b6eb4fe8cfb2312d062ce6b6
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-08-24 16:14:46 -0300

    Initial Commit

---
+++ Backend/Model/Aluno.js
@@ -0,0 +1,13 @@
+class Aluno {
+    nome;
+    idade;
+    curso;
+    periodoIngresso;
+
+    constructor(nome, idade, curso, periodoIngresso){
+        this.nome = nome
+        this.idade = idade
+        this.curso = curso
+        this.periodoIngresso = periodoIngresso
+    }
+}
