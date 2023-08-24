ecc6b9d4b05f5da3d1e9835c24cf455888212f0b
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-08-24 16:24:30 -0300

    feat: add getters to Aluno class

--- Backend/Model/Aluno.js
+++ Backend/Model/Aluno.js
@@ -1,13 +1,31 @@
 class Aluno {
-    nome;
-    idade;
-    curso;
-    periodoIngresso;
+    _nome;
+    _idade;
+    _curso;
+    _periodoIngresso;
 
     constructor(nome, idade, curso, periodoIngresso){
-        this.nome = nome
-        this.idade = idade
-        this.curso = curso
-        this.periodoIngresso = periodoIngresso
+        this._nome = nome
+        this._idade = idade
+        this._curso = curso
+        this._periodoIngresso = periodoIngresso
     }
+
+    getNome(){
+        return this._nome
+    }
+
+    getIdade(){
+        return this._idade
+    }
+
+    getCurso(){
+        return this._curso
+    }
+
+    getPeriodoIngresso(){
+        return this._periodoIngresso
+    }
+
+
 }
