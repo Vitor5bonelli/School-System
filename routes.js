e9c6bda5400690ca8303a290fddacc88e77a72d9
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-08-24 23:48:03 -0300

    feat: add 404 error page

--- routes.js
+++ routes.js
@@ -1,11 +1,17 @@
 const port = 55155
 const express = require("express")
+const path = require("path")
 const app = express()
 
 app.use(express.static("Frontend"))
 
 app.get("/", (req, res) => {
-    res.sendFile("./Frontend/views/index.html", {root: __dirname})
+    //res.sendFile("./Frontend/views/index.html", {root: __dirname})
+    res.sendFile(path.join(__dirname, "Frontend/views", "index.html"))
+})
+
+app.get("/*", (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, "Frontend/views", "404.html"))
 })
 
 app.listen(port, ()  => {
